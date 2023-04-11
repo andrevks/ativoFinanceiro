a139f473f47e16c173120360d2716cfd70e9b4fa
andrevks <andregp019@gmail.com>
2023-04-10 21:45:19 -0400

    build: yahoo finance setup

--- app/Controllers/Http/AssetsController.ts
+++ app/Controllers/Http/AssetsController.ts
@@ -1,37 +1,22 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import yahoo from 'yahoo-finance'
 export default class AssetsController {
   public async index({}: HttpContextContract) {
-    return {
-      assets: [
-        {
-          id: 1,
-          name: 'Bitcoin',
-          symbol: 'BTC',
-          slug: 'bitcoin',
-          num_market_pairs: 9388,
-          date_added: '2013-04-28T00:00:00.000Z',
-          tags: ['mineable'],
-          max_supply: 21000000,
-          circulating_supply: 18718737,
-          total_supply: 18718737,
-          platform: null,
-          cmc_rank: 1,
-          last_updated: '2021-05-05T20:02:02.000Z',
-        },
-      ],
-    }
+    const data = await yahoo.historical(
+      {
+        symbol: 'BBD',
+        from: '2020-01-01',
+        to: '2020-04-20',
+        period: 'd',
+      },
+      (err, quotes) => {
+        if (err) {
+          console.log(err)
+        }
+        return quotes
+      }
+    )
+
+    return data
   }
-
-  public async create({}: HttpContextContract) {}
-
-  public async store({}: HttpContextContract) {}
-
-  public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
 }
