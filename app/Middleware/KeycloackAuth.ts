7b7f3d8d20400363b12f5b6c9327b22d6c45a330
andrevks <andregp019@gmail.com>
2023-05-08 18:17:10 -0400

    build: login and protected keycloack middleware

---
+++ app/Middleware/KeycloackAuth.ts
@@ -0,0 +1,33 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Env from '@ioc:Adonis/Core/Env'
+import Token from 'keycloak-connect/middleware/auth-utils/token'
+import Signature from 'keycloak-connect/middleware/auth-utils/signature'
+
+export default class KeycloakAuth {
+  public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
+    try {
+      // Validate token with Keycloak
+      const headers = request.headers()
+      if (!headers.authorization) {
+        return response.status(401).send({ message: 'Unauthorized' })
+      }
+      const tokenRaw = headers?.authorization?.replace('Bearer ', '')
+      const token = new Token(tokenRaw, 'adonis-web')
+      const signature = new Signature({
+        realmUrl: Env.get('KEYCLOACK_REALM_URL'),
+        publicKey: Env.get('KEYCLOACK_PUBLIC_KEY'),
+        minTimeBetweenJwksRequests: 0,
+      })
+
+      const signatureResult = await signature.verify(token, null)
+      if (!signatureResult) {
+        return response.status(401).send({ message: 'Unauthorized', data: 'Invalid token' })
+      }
+
+      await next()
+    } catch (error) {
+      console.error(error)
+      return response.status(401).send({ message: 'Unauthorized' })
+    }
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,35 @@
 import Route from '@ioc:Adonis/Core/Route'
+import Env from '@ioc:Adonis/Core/Env'
+import axios from 'axios'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.get('/protected', async () => {
+  return 'Protected route'
+}).middleware(['keycloackAuth'])
+
+Route.get('/login', async () => {
+  const tokenUrl = 'http://localhost:8080/realms/demo/protocol/openid-connect/token'
+  const keycloackConfig = {
+    grant_type: 'client_credentials',
+    client_id: Env.get('KEYCLOAK_CLIENT_ID'),
+    client_secret: Env.get('KEYCLOACK_SECRET'),
+  }
+  try {
+    const { data } = await axios.post(tokenUrl, keycloackConfig, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+
+    return data
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 })
 
 Route.resource('assets', 'AssetsController').apiOnly().only(['index'])
